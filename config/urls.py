abcb2c88274c7e4a54d5440287ddc916795d2600
R.Sanjar <sanjarruzmanov999@gmail.com>
2025-12-22 23:51:33 +0500

    order to'girlandi

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({"status": "API is running"})),
+
     path('admin/', admin.site.urls),
 
     # Accounts / Authentication
